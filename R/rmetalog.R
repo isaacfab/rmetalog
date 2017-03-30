89612eb04b0708cf84ccd884bc3718997193eaf4
Isaac Faber <isaacfab@gmail.com>
2017-03-30 15:16:33 -0700

    Adding intal scripts and README

---
+++ R/rmetalog.R
@@ -0,0 +1,7 @@
+#will use this file for inital metalog functions
+
+
+#need a function to take emperical inputs and return a distribution
+
+
+#need a fuction that retuns a cdf function from an emperical input
